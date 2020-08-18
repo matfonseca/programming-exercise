f12b7e6af718f2e38f657756bb1f480e7bb2fb14
matias fonseca <fonsecamatiasm@gmail.com>
2020-08-18 19:24:52 -0300

    Agrego ejercicio mensaje

--- 2018/main.py
+++ 2018/main.py
@@ -1,5 +1,6 @@
 # Given a list of integers L and a value K,
 # return the number of pairs whose elements sum to less than K.
+print("--Pair Sum--")
 
 array = [3, 7, 2, 8]
 k = 10
@@ -16,3 +17,24 @@ def sumsLessThan(list_numbers, k):
 
 
 print(sumsLessThan(array, k))
+
+
+print("--Man in the Middle--")
+
+message = []
+
+with open('exercise_3.txt', 'r') as f:
+    index_line = 0
+    for line in f:
+        if index_line != 0:
+           message.append(line.strip())
+        index_line += 1
+
+encode_label_message = "It Is Up To You#####"
+
+def encodeMessage(message):
+    for line in message:
+        print(encode_label_message + line)
+
+
+encodeMessage(message)
